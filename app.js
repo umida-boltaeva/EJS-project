3965cfb3462a32703a29f180cf321d1c2cd6f9f8
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-02-20 16:19:15 +1100

    my first template code!

--- app.js
+++ app.js
@@ -2,19 +2,39 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
     
     var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
 
-    var currentDay = today.getDate();
-
-    if(currentDay === 6 || currentDay === 0){
-        res.send("Yaay! It's the weekend!");
-    } else {
-        res.send("Boo! I have to work!");
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
     }
+
+    res.render("list", {kindOfDay: day});
 });
 
 app.listen(3000);
