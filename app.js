fe8904cd51607e651acf9410904055b4914646b4
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-02-23 15:18:50 +1100

    add new item to the list

--- app.js
+++ app.js
@@ -2,39 +2,34 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+let items = ["Buy food", "Cook food", "Eat food"];
+
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
     
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    let today = new Date();
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-    }
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    res.render("list", {kindOfDay: day});
+    let day = today.toLocaleDateString("en-AU", options);
+
+    res.render("list", {kindOfDay: day, newListItems: items});
 });
+
+app.post("/", function(req, res){
+    let item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+    
+})
 
 app.listen(3000);
