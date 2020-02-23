1e9b84b3a9760780473759d5c8d34232d3486b31
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-02-24 10:02:48 +1100

    add layouts! Cool!

--- app.js
+++ app.js
@@ -44,11 +44,8 @@ app.get("/work", function(req, res){
     res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
-// app.post("/work", function(req, res){
-//     let item = req.body.newItem;
-//     workItems.push(item);
-
-//     res.redirect("/work");
-// });
+app.get("/about", function(req, res){
+    res.render("about");
+})
 
 app.listen(3000);
