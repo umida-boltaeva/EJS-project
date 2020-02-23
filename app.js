79975c7f8c5ab1e4d84ca60f1b15d5dad954429b
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-02-23 15:53:19 +1100

    here is a beautiful todo list page

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let items = ["Buy food", "Cook food", "Eat food"];
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(express.static("public"));
+
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
