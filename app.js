cbd56866806d51f64721668d901feb28abd7061f
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-02-24 11:02:06 +1100

    module export and final refactor code

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const date = require(__dirname + "/date.js");
 const app = express();
 
-let items = ["Buy food", "Cook food", "Eat food"];
+const items = ["Buy food", "Cook food", "Eat food"];
 
-let workItems = [];
+const workItems = [];
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -14,21 +16,13 @@ app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
     
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-AU", options);
+    const day = date.getDate();
 
     res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
     if(req.body.list ==="Work"){
         workItems.push(item);
